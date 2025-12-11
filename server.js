7fb19b989f82fdaa6a248fbac2385e150884440c
Dathutech27 <dathutech27@gmail.com>
2025-12-12 04:05:39 +0700

    Fix: Remove duplicate express require in server.js

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const port = process.env.PORT || 4000
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: 'https://dafastfood.vercel.app', // Tên miền Vercel của bạn
+    // Thêm URL Admin vào danh sách
+    origin: ['https://dafastfood.vercel.app', 'https://adminfastfood.vercel.app'], 
     credentials: true
 }))
 
